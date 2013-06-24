83ac9f184d2138a2f3fd7ba1fd414866b32e1fda
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-06-24 12:29:23 +1200

    Add tests for instance's correctness.

--- svg/tests/create_object_test.py
+++ svg/tests/create_object_test.py
@@ -14,3 +14,17 @@ class CreateSvgImageInstanceTestCase(unittest.TestCase):
         t = tools.get_svg_text()
         icon = SvgImage(t)
         self.assertIsInstance(icon, SvgImage)
+
+
+class CreateSvgImageDataTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.icon = SvgImage(tools.get_svg_file())
+        self.root = self.icon._SvgImage__root
+        self.xmlns = self.icon._SvgImage__xlmns
+
+    def test_root(self):
+        self.assertEqual(self.root.tag, '{http://www.w3.org/2000/svg}svg')
+
+    def test_xmlns(self):
+        self.assertEqual(self.xmlns, '{http://www.w3.org/2000/svg}')
